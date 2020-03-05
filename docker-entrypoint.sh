ab705ff2d1e54e73921955361b04a0402b8636fa
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-03-05 16:37:28 +0200

    Refs #114576 Modified script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,9 @@ REVISION=$(jq '.puppeteer.chromium_revision' node_modules/puppeteer/package.json
 SANDBOX_PATH_CHR="node_modules/puppeteer/.local-chromium/linux-${REVISION}/chrome-linux"
 cd ${SANDBOX_PATH_CHR}
 
-chown root:root chrome_sandbox
-chmod 4755 chrome_sandbox
+cp chrome_sandbox chrome-sandbox
+chown root:root chrome-sandbox
+chmod 4755 chrome-sandbox
+
 # copy sandbox executable to a shared location
-cp -p chrome_sandbox /usr/local/sbin/chrome-devel-sandbox
+# cp -p chrome-sandbox /usr/local/sbin/chrome-sandbox
